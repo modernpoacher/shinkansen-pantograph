544410417c81b13dfbb181f72d40bea3ba943342
Jonathan Perry <modernpoacher@modernpoacher.com>
2017-06-20 18:57:00 +0100

    Boiler

--- src/valves/index.js
+++ src/valves/index.js
@@ -1,17 +1,10 @@
 import {
-  Steam
-} from 'react-steam'
+  Boiler
+} from 'react-boiler'
 
 import {
   Gears
 } from 'react-gears'
-
-const {
-  ALPHA: ALPHA_STATE,
-  DEBARK: DEBARK_STATE,
-  EMBARK: EMBARK_STATE,
-  OMEGA: OMEGA_STATE
-} = Steam
 
 const ALPHA = {
   CHANGE: 'ALPHA_CHANGE',
@@ -20,10 +13,11 @@ const ALPHA = {
   QUERY: 'ALPHA_QUERY'
 }
 
-const DEBARK = {
-  ROUTE: 'DEBARK_ROUTE',
-  FETCH: 'DEBARK_FETCH',
-  STORE: 'DEBARK_STORE'
+const OMEGA = {
+  CHANGE: 'OMEGA_CHANGE',
+  FETCH: 'OMEGA_FETCH',
+  STORE: 'OMEGA_STORE',
+  QUERY: 'OMEGA_QUERY'
 }
 
 const EMBARK = {
@@ -32,11 +26,10 @@ const EMBARK = {
   STORE: 'EMBARK_STORE',
 }
 
-const OMEGA = {
-  CHANGE: 'OMEGA_CHANGE',
-  FETCH: 'OMEGA_FETCH',
-  STORE: 'OMEGA_STORE',
-  QUERY: 'OMEGA_QUERY'
+const DEBARK = {
+  ROUTE: 'DEBARK_ROUTE',
+  FETCH: 'DEBARK_FETCH',
+  STORE: 'DEBARK_STORE'
 }
 
 const ACTION = {}
@@ -48,12 +41,33 @@ const HISTORY = {
   getCurrentLocation: () => {}
 }
 
-function REDIRECT_ROUTE ({ history, route }) { // console.log('REDIRECT_ROUTE', route) // eslint-disable-line
+function REDIRECT_TO_ALPHA ({ history, alpha }) { // console.log('REDIRECT_TO_ALPHA', route) // eslint-disable-line
   const {
     pathname: PATHNAME
   } = history.getCurrentLocation()
 
-  const pathname = Gears.path(route)
+  const pathname = Gears.path({ [Boiler.ALPHA]: alpha }, Boiler.ALPHA_PATTERN)
+
+  if (PATHNAME !== pathname) history.push(pathname)
+}
+
+function REDIRECT_TO_OMEGA ({ history, alpha, omega }) { // console.log('REDIRECT_TO_OMEGA', route) // eslint-disable-line
+  const {
+    pathname: PATHNAME
+  } = history.getCurrentLocation()
+
+  const pathname = Gears.path({ [Boiler.ALPHA]: alpha, [Boiler.OMEGA]: omega }, Boiler.OMEGA_PATTERN)
+
+  if (PATHNAME !== pathname) history.push(pathname)
+}
+
+
+function REDIRECT_TO_EMBARK ({ history, embark }) { // console.log('REDIRECT_TO_EMBARK', embark) // eslint-disable-line
+  const {
+    pathname: PATHNAME
+  } = history.getCurrentLocation()
+
+  const pathname = Gears.path({ [Boiler.EMBARK]: embark }, Boiler.EMBARK_PATTERN)
 
   if (PATHNAME !== pathname) history.push(pathname)
 }
@@ -63,71 +77,65 @@ function REDIRECT_TO_DEBARK ({ history, debark }) { // console.log('REDIRECT_TO_
     pathname: PATHNAME
   } = history.getCurrentLocation()
 
-  const pathname = `/${debark}`
+  const pathname = Gears.path({ [Boiler.DEBARK]: debark }, Boiler.DEBARK_PATTERN)
 
   if (PATHNAME !== pathname) history.push(pathname)
 }
 
-function REDIRECT_TO_EMBARK ({ history, embark }) { // console.log('REDIRECT_TO_EMBARK', embark) // eslint-disable-line
-  const {
-    pathname: PATHNAME
-  } = history.getCurrentLocation()
-
-  const pathname = `/${embark}`
-
-  if (PATHNAME !== pathname) history.push(pathname)
-}
-
-function REDIRECT ({ [ALPHA_STATE]: alpha, [OMEGA_STATE]: omega, [DEBARK_STATE]: debark, [EMBARK_STATE]: embark, history }) { // console.log('REDIRECT()', alpha, omega, debark, embark) // eslint-disable-line
+function REDIRECT ({ [Boiler.ALPHA]: alpha, [Boiler.OMEGA]: omega, [Boiler.EMBARK]: embark, [Boiler.DEBARK]: debark, history }) { // console.log('REDIRECT()', alpha, omega, debark, embark) // eslint-disable-line
   if (alpha && omega) {
-    REDIRECT_ROUTE({ route: { alpha, omega }, history })
-  } else if (debark) {
-    REDIRECT_TO_DEBARK({ debark, history })
+    REDIRECT_TO_OMEGA({ alpha, omega, history })
   } else if (embark) {
     REDIRECT_TO_EMBARK({ embark, history })
+  } else if (debark) {
+    REDIRECT_TO_DEBARK({ debark, history })
+  } else if (alpha) {
+    REDIRECT_TO_ALPHA({ alpha, history })
+  } else if (omega) {
+    throw Error('Valves encountered a route error in REDIRECT()')
   }
 }
 
-function redirectFromAlpha ({ [ALPHA_STATE]: { redirect }, history }) { // console.log('redirectFromAlpha()', redirect) // eslint-disable-line
+function redirectFromAlpha ({ [Boiler.ALPHA]: { redirect }, history }) { // console.log('redirectFromAlpha()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
-function redirectFromDebark ({ [DEBARK_STATE]: { redirect }, history }) { // console.log('redirectFromDebark()', redirect) // eslint-disable-line
+function redirectFromOmega ({ [Boiler.OMEGA]: { redirect }, history }) { // console.log('redirectFromOmega()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
-function redirectFromEmbark ({ [EMBARK_STATE]: { redirect }, history }) { // console.log('redirectFromEmbark()', redirect) // eslint-disable-line
+function redirectFromEmbark ({ [Boiler.EMBARK]: { redirect }, history }) { // console.log('redirectFromEmbark()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
-function redirectFromOmega ({ [OMEGA_STATE]: { redirect }, history }) { // console.log('redirectFromOmega()', redirect) // eslint-disable-line
+function redirectFromDebark ({ [Boiler.DEBARK]: { redirect }, history }) { // console.log('redirectFromDebark()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
 function poppet ({ action: { type, ...rest } = ACTION, store = STORE, history = HISTORY }) {
   switch (type) {
-    case OMEGA.QUERY: // console.log('Valves.poppet():OMEGA.QUERY', OMEGA.QUERY, { ...rest, type }) // eslint-disable-line
+    case 'OMEGA_QUERY': // console.log('Valves.poppet():OMEGA_QUERY', { ...rest, type }) // eslint-disable-line
       {
         const state = store.getState()
 
         redirectFromOmega({ ...state, history })
       }
       break
-    case EMBARK.ROUTE: // console.log('Valves.poppet():EMBARK.ROUTE', EMBARK.ROUTE, { ...rest, type }) // eslint-disable-line
+    case 'EMBARK_ROUTE': // console.log('Valves.poppet():EMBARK_ROUTE', { ...rest, type }) // eslint-disable-line
       {
         const state = store.getState()
 
         redirectFromEmbark({ ...state, history })
       }
       break
-    case DEBARK.ROUTE: // console.log('Valves.poppet():DEBARK.ROUTE', DEBARK.ROUTE, { ...rest, type }) // eslint-disable-line
+    case 'DEBARK_ROUTE': // console.log('Valves.poppet():DEBARK_ROUTE', { ...rest, type }) // eslint-disable-line
       {
         const state = store.getState()
 
         redirectFromDebark({ ...state, history })
       }
       break
-    case ALPHA.QUERY: // console.log('Valves.poppet():ALPHA.QUERY', ALPHA.QUERY, { ...rest, type }) // eslint-disable-line
+    case 'ALPHA_QUERY': // console.log('Valves.poppet():ALPHA_QUERY', { ...rest, type }) // eslint-disable-line
       {
         const state = store.getState()
 
