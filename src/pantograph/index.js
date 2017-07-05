b36206c549f52d01d45f89ddb2027cf372f5208a
Jonathan Perry <modernpoacher@modernpoacher.com>
2017-07-05 19:50:13 +0100

    Exception

--- src/pantograph/index.js
+++ src/pantograph/index.js
@@ -81,72 +81,60 @@ function REDIRECT_TO_DEBARK ({ history, debark }) { // console.log('REDIRECT_TO_
   if (PATHNAME !== pathname) history.push(pathname)
 }
 
-function REDIRECT_TO_EXCEPTION ({ history, exception }) { // console.log('REDIRECT_TO_DEBARK', debark) // eslint-disable-line
-  const {
-    pathname: PATHNAME
-  } = history.getCurrentLocation()
-
-  const pathname = Rails.path({ [Signals.EXCEPTION]: exception }, Signals.EXCEPTION_PATTERN)
-
-  if (PATHNAME !== pathname) history.push(pathname)
-}
-
-function REDIRECT ({ [Signals.ALPHA]: alpha, [Signals.OMEGA]: omega, [Signals.EMBARK]: embark, [Signals.DEBARK]: debark, exception, history }) { // console.log('REDIRECT()', alpha, omega, debark, embark) // eslint-disable-line
+function REDIRECT ({ [Signals.ALPHA]: alpha, [Signals.OMEGA]: omega, [Signals.EMBARK]: embark, [Signals.DEBARK]: debark, exception, history }) { // console.log('REDIRECT()', alpha, omega, embark, debark, exception) // eslint-disable-line
   if (alpha && omega) {
     REDIRECT_TO_OMEGA({ alpha, omega, history })
   } else if (embark) {
     REDIRECT_TO_EMBARK({ embark, history })
   } else if (debark) {
     REDIRECT_TO_DEBARK({ debark, history })
-  } else if (exception) {
-    REDIRECT_TO_EXCEPTION({ exception, history })
   } else if (alpha) { // can appear on its own
     REDIRECT_TO_ALPHA({ alpha, history })
-  } else if (omega) { // should never appear on its own
+  } else if (omega) { // can not appear on its own
     throw Error('Pantograph encountered a route error in REDIRECT()')
   }
 }
 
-function redirectFromAlpha ({ [Signals.ALPHA]: { redirect }, history }) { // console.log('redirectFromAlpha()', redirect) // eslint-disable-line
+function redirectFromAlpha ({ [Signals.ALPHA]: { redirect } = {}, history }) { // console.log('redirectFromAlpha()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
-function redirectFromOmega ({ [Signals.OMEGA]: { redirect }, history }) { // console.log('redirectFromOmega()', redirect) // eslint-disable-line
+function redirectFromOmega ({ [Signals.OMEGA]: { redirect } = {}, history }) { // console.log('redirectFromOmega()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
-function redirectFromEmbark ({ [Signals.EMBARK]: { redirect }, history }) { // console.log('redirectFromEmbark()', redirect) // eslint-disable-line
+function redirectFromEmbark ({ [Signals.EMBARK]: { redirect } = {}, history }) { // console.log('redirectFromEmbark()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
-function redirectFromDebark ({ [Signals.DEBARK]: { redirect }, history }) { // console.log('redirectFromDebark()', redirect) // eslint-disable-line
+function redirectFromDebark ({ [Signals.DEBARK]: { redirect } = {}, history }) { // console.log('redirectFromDebark()', redirect) // eslint-disable-line
   if (redirect) REDIRECT({ ...redirect, history })
 }
 
-function graphite ({ action: { type, ...rest } = ACTION, store = STORE, history = HISTORY }) {
+function graphite ({ action: { type, ...action } = ACTION, store = STORE, history = HISTORY }) {
   switch (type) {
-    case 'OMEGA_QUERY': // console.log('Pantograph.graphite():OMEGA_QUERY', { ...rest, type }) // eslint-disable-line
+    case 'OMEGA_QUERY': // console.log('Pantograph.graphite():OMEGA_QUERY', { ...action, type }) // eslint-disable-line
       {
         const state = store.getState()
 
         redirectFromOmega({ ...state, history })
       }
       break
-    case 'EMBARK_ROUTE': // console.log('Pantograph.graphite():EMBARK_ROUTE', { ...rest, type }) // eslint-disable-line
+    case 'EMBARK_ROUTE': // console.log('Pantograph.graphite():EMBARK_ROUTE', { ...action, type }) // eslint-disable-line
       {
         const state = store.getState()
 
         redirectFromEmbark({ ...state, history })
       }
       break
-    case 'DEBARK_ROUTE': // console.log('Pantograph.graphite():DEBARK_ROUTE', { ...rest, type }) // eslint-disable-line
+    case 'DEBARK_ROUTE': // console.log('Pantograph.graphite():DEBARK_ROUTE', { ...action, type }) // eslint-disable-line
       {
         const state = store.getState()
 
         redirectFromDebark({ ...state, history })
       }
       break
-    case 'ALPHA_QUERY': // console.log('Pantograph.graphite():ALPHA_QUERY', { ...rest, type }) // eslint-disable-line
+    case 'ALPHA_QUERY': // console.log('Pantograph.graphite():ALPHA_QUERY', { ...action, type }) // eslint-disable-line
       {
         const state = store.getState()
 
@@ -155,7 +143,7 @@ function graphite ({ action: { type, ...rest } = ACTION, store = STORE, history 
       break
   }
 
-  return { ...rest, type }
+  return { ...action, type }
 }
 
 export class Pantograph {
