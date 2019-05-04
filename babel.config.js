56067d7859dafcadd74057a9e6bbfbb4f72082c0
Jonathan Perry <modernpoacher@modernpoacher.com>
2019-05-04 19:36:06 +0100

    Outdated

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  compact: true,
+  comments: false,
+  presets: [
+    [
+      '@babel/env', {
+        useBuiltIns: 'entry',
+        targets: {
+          node: 'current',
+          browsers: [
+            'last 2 versions'
+          ]
+        },
+        corejs: '3.0.1'
+      }
+    ]
+  ],
+  'plugins': [
+    '@babel/proposal-export-default-from',
+    '@babel/proposal-class-properties'
+  ]
+}
