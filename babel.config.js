c3ee459cd17db9097a7827624423a5486bbf1d55
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 19:04:53 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,57 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        useBuiltIns: 'entry',
-        targets: {
-          node: 'current',
-          browsers: [
-            'last 2 versions'
-          ]
-        },
-        corejs: 3
-      }
-    ]
-  ],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    '@babel/proposal-class-properties',
-    [
-      'module-resolver', {
-        alias: {
-          'shinkansen-pantograph': './src'
-        }
-      }
-    ]
+const debug = require('debug')
+
+const log = debug('shinkansen')
+
+const {
+  env: {
+    NODE_ENV = 'development'
+  }
+} = process
+
+log('`shinkansen` is awake')
+
+function env () {
+  log({ NODE_ENV })
+
+  return (
+    NODE_ENV === 'production'
+  )
+}
+
+const presets = [
+  [
+    '@babel/env', {
+      useBuiltIns: 'entry',
+      targets: {
+        node: 'current',
+        browsers: [
+          'last 2 versions'
+        ]
+      },
+      corejs: 3
+    }
   ]
+]
+
+const plugins = [
+  '@babel/proposal-export-default-from',
+  '@babel/proposal-class-properties',
+  [
+    'module-resolver', {
+      alias: {
+        'shinkansen-pantograph': './src'
+      }
+    }
+  ]
+]
+
+module.exports = (api) => {
+  if (api) api.cache.using(env)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
